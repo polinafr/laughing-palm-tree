63ddb61b91250994dd0eb533d9137ebbad208758
Polina <polina.korogodska@gmail.com>
2021-08-16 14:41:56 +0300

    IBL draft

---
+++ BLL/IBL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BELayer;
+using DAL;
+
+namespace BLL
+{
+    enum GraphType { Columns, Pie};
+    interface IBL
+    {
+        
+        bool saveToPDF(Dictionary<Good, float> statistics, GraphType type);
+        List<List<Good>> getRecommendations();
+        Dictionary<float, Good> GetGoodsRatio();
+        Dictionary<int, Good> GetBoughtGoodsQuantity();
+    }
+}
